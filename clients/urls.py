f63604c91292594046bb5eb78cc50c0e158f90eb
xDaBix <21bmiit091@gmail.com>
2025-03-26 21:05:08 +0530

    26-03-25 pt 2

--- clients/urls.py
+++ clients/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path,include
 from . import views
@@ -10,13 +9,9 @@ urlpatterns = [
     path('logout/',views.logout_view,name="logout"),
     path('signup/',views.signup,name="signup"),
     path('verifyotp/',views.verifyotp,name="verifyotp"),
-    path('create-room/', views.create_room, name='create_room'),  
+    path('create-room/', views.create_room, name='create_room'),
     path('createroom/', views.createroom, name='createroom'),
     path('roomdetail/<int:id>/',views.roomdetail,name="roomdetail"),
-    path('join/<int:room_id>/', views.join_room, name='join_room'),
+    path('join/<int:roomid>/', views.join_room, name='join_room'),
 
-    
-
-
-    
 ]
